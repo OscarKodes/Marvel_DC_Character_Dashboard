8a0a7912c00dd04e169b8f63e786d25499a9a4ac
Oscar Ko <OscarKoEmail@gmail.com>
2022-04-22 20:04:00 -0400

    Assign colors with an obj dictionary, sort bars by property count, fix data identifier to dynamic bar property

--- barChart.js
+++ barChart.js
@@ -35,15 +35,14 @@ class BarChart {
       filteredData = d3.groups(filteredData, d => d[barKey]).map(d => {
 
         return {property: d[0], count: d[1].length};
-      })
+      }).sort((a, b) => b.count - a.count);
 
 
 
       // SCALES =======================================
       const xScale = d3.scaleLinear()
-        .domain([0, d3.max(filteredData, d => d.count)])
-        .range([0, this.width - this.margin * 2])
-        .nice()
+        .domain([0, d3.max(filteredData, d => d.count)]).nice()
+        .range([0, this.width - this.margin * 2]).nice()
   
       const yScale = d3.scaleBand()
           .domain(filteredData.map(d => d.property))
@@ -62,37 +61,43 @@ class BarChart {
       // !!! REMEMBER TO UPDATE COLORS TO THE NEW GROUPINGS
 
       const colorRange = {
-        eye: [
-          'yellow', 'blue', 'green', 
-          'brown', 'red', 'white', 
-          'black', 'silver', 'magenta', 
-          'purple'
-        ],
-        hair: [
-          'navy', 'blond', 'brown', 
-          'red', 'blue', 'green', 
-          'white', 'purple', 'magenta', 
-          'silver'
-        ],
-        race: [
-          'navy', 'blond', 'brown', 
-          'red', 'blue', 'green', 
-          'white', 'purple', 'magenta'
-        ],
+        'yellow': "yellow",
+        'blue': "blue",
+        'green': "green",
+        'brown': "brown",
+        'red': "red",
+        'white': "white",
+        'black': "black",
+        'silver': "silver",
+        'mixed': "magenta",
+        'purple': "purple",
+        "no hair": "lavender",
+        "blond": "yellow",
+        "burnette": "brown",
+        "human": "tan",
+        "alien": "green",
+        "amazon/atlantean": "cerulean",
+        "diety": "yellow",
+        "demon": "purple",
+        "undead": "brown",
+        "mixed-race": "magenta",
+        "animal": "orange"
       }
+
+
   
-      const colorScale = d3.scaleOrdinal(propertyArr, colorRange[barKey]);
+      const colorScale = d3.scaleOrdinal(propertyArr, propertyArr.map(d => colorRange[d]));
 
       
       // DRAW BARS =====================================
       const bars = this.svg
-        .selectAll("g.bar")
-        .data(filteredData, d => d.color)
+        .selectAll(`g.${barKey}-bar`)
+        .data(filteredData, d => d.property)
         .join(
           enter =>
             enter
               .append("g")
-              .attr("class", "bar")
+              .attr("class", `${barKey}-bar`)
               .call(enter => enter.append("rect")),
               // .call(enter => enter.append("text")),
           update => update,
